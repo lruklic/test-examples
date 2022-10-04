47540f4a3749fad02c4152d9fe6456c4bf421390
Luka Ruklić <ruklic.luka@gmail.com>
2022-10-04 18:44:01 +0200

    gadmin - add fetch for device config log

--- gateway-admin/js/control.js
+++ gateway-admin/js/control.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $("#fetch-data").on("click", function() {
         $.ajax({
-            url: "172.16.0.1:8080/conf_list",
+            url: "http://172.16.0.1:8080/conf_list",
             type: "GET",
             success: function(data) {
-                console.log(data);
+                $("#conf-list").html(data);
             }
         });
     });
