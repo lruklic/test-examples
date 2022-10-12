07b09e7af02043515f08f53b3d3f7e1a1c0f406d
Luka Ruklić <ruklic.luka@gmail.com>
2022-10-12 15:03:13 +0200

    gadmin - add socket and fetch fields and mechanism

--- gateway-admin/js/control.js
+++ gateway-admin/js/control.js
@@ -1,13 +1,50 @@
 $(document).ready(function() {
-    $("#fetch-data").on("click", function() {
-        $.ajax({
-            url: "http://172.16.0.1:8080/conf_list",
-            type: "GET",
-            success: function(data) {
-                $("#conf-list").html(data);
-            }
-        });
+    
+    $(".refresh").on("click", function() {
+        fetch($(this).attr('id'));
+
     });
 
+    $("#open-ws").on("click", function() {
+        socket();
+    });
     
 });
+
+function fetch(id) {
+    $.ajax({
+        url: "http://172.16.0.1:8080/" + id,
+        type: "GET",
+        success: function(data) {
+            // check if typeof string
+            if (typeof data === "string") {
+                $("#fetch-" + id).html(data);
+            } else {
+                $("#fetch-" + id).html(JSON.stringify(data, undefined, 4));
+            }
+        }
+    });
+}
+
+function socket() {
+    if ("WebSocket" in window) {
+        console.log("WebSocket is supported by your Browser!");
+        
+        // Let us open a web socket
+        var ws = new WebSocket("ws://172.16.0.1:8080/ws");
+        
+        ws.onopen = function() {
+            document.getElementById("ws-log").value = "Connection is established... \n";
+            ws.send("ping");
+        };
+
+        ws.onmessage = function (evt) {
+            let message = evt.timeStamp.toFixed(0) + "\t" + evt.data.replace("Luka", "Tomica") + "\n";
+            document.getElementById("ws-log").value += message; 
+        }
+
+        ws.onclose = function() {
+            document.getElementById("ws-log").value += "Connection is closed...\n";
+        }
+    }
+}
