ceab8653f513a6739866a45c67af411ba2f04a79
Luka <ruklic.luka@gmail.com>
2022-10-19 13:09:21 +0200

    simplews - implement simple REST server in Go

---
+++ simple-ws/go.mod
@@ -0,0 +1,3 @@
+module github.com/piesocket/go-websocket-server
+
+go 1.19
---
+++ simple-ws/main.go
@@ -0,0 +1,25 @@
+package main
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+}
+	
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello WebSocket")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	fmt.Println("Hello World")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
